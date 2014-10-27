8ab2b935bf99859c0934b3f7d99214df6fe408cb
Morita Kohei <yosupo06@gmail.com>
2014-10-27 15:51:05 +0900

    速度の向上

--- Dlang/DataStruct/rbstree.d
+++ Dlang/DataStruct/rbstree.d
@@ -1,63 +1,55 @@
-class RBSTree(T) {
+struct RBSTree(T) {
     alias Node = RBSNode*;
-    static int size(in Node x) {
-        return ((x is null) ? 0 : x.sz);
-    }
-
+    __gshared RBSNode last_d = RBSNode(null, null, 0, 0);
+    __gshared Node last = &last_d;
+    
     static Node update(Node x) {
-        x.sz = size(x.l) + size(x.r) + 1;
+        x.sz = x.l.sz + x.r.sz + 1;
         return x;
     }
 
-    static Node merge(Node l, Node r) {
-        Node res;
-        Node *root = &res;
-        int ls, rs;
-        while (true) {
-            if (l is null) {
-                *root = r;
-                break;
-            }
-            if (r is null) {
-                *root = l;
-                break;
-            }
-            ls = l.sz, rs = r.sz;
-            if (uniform(0, ls+rs, RBSNode.rnd) < ls) {
-                l.sz += rs;
-                *root = l;
-                root = &l.r;
-                l = l.r;
-            } else {
-                r.sz += ls;
-                *root = r;
-                root = &r.l;
-                r = r.l;
-            }
+    static void push(Node x) {
+        if (x.rev) {
+            swap(x.l, x.r);
+            if (x.l.sz) x.l.rev ^= true;
+            if (x.r.sz) x.r.rev ^= true;
+            x.rev = false;
         }
-        return res;
+    }
+
+    static Node merge(Node l, Node r) {
+        if (!l.sz) return r;
+        if (!r.sz) return l;
+        push(l); push(r);
+        auto ls = l.sz, rs = r.sz;
+        static uint xor128(){
+            __gshared uint x=123456789,y=362436069,z=521288629,w=88675123;
+            uint t;
+            t=(x^(x<<11));x=y;y=z;z=w; return( w=(w^(w>>19))^(t^(t>>8)) );
+        }
+        if (xor128() % (ls+rs) < ls) {
+            l.r = merge(l.r, r);
+            return update(l);
+        } else {
+            r.l = merge(l, r.l);
+            return update(r);
+        }
     }
 
     static Node[2] split(Node x, int k) {
-        Node[2] res;
-        Node*[2] root = [&res[0], &res[1]];
-        while (k) {
-            auto ls = size(x.l);
-            if (k <= ls) { 
-                x.sz -= k;
-                *root[1] = x;
-                root[1] = &x.l;
-                x = x.l;
-            } else {
-                x.sz = k;
-                *root[0] = x;
-                root[0] = &x.r;
-                x = x.r; k -= ls+1;                
-            }
+        if (!x.sz) return [last, last];
+        push(x);
+        if (k <= x.l.sz) {
+            auto y = split(x.l, k);
+            x.l = y[1];
+            y[1] = update(x);
+            return y;
+        } else {
+            auto y = split(x.r, k-x.l.sz-1);
+            x.r = y[0];
+            y[0] = update(x);
+            return y;
         }
-        *root[0] = null;
-        *root[1] = x;
-        return res;
     }
 
     static Node erase(Node x, int k) {
@@ -66,168 +58,125 @@ class RBSTree(T) {
         return x;
     }
 
-    static Node insert(Node x, int k, T d) {    
+    static Node insert(Node x, int k, T d) {
         auto y = split(x, k);
         return merge(merge(y[0], new RBSNode(d)), y[1]);
     }
 
-    static struct RBSNode {
-        static Xorshift rnd;
-        static this() {
-            rnd = Xorshift(unpredictableSeed);
-        }
+    static Node built(T[] d) {
+        if (d.length == 0) return last;
+        int u = cast(int)d.length/2;
+        return new RBSNode(built(d[0..u]), built(d[u+1..$]), d[u]);
+    }
 
+    static struct RBSNode {
         Node l, r;
         int sz;
         T value;
-
+        bool rev;
         this(T d) {
             sz = 1; value = d;
         }
 
-        this(T[] d) {
-            sz = cast(int)d.length;
-            value = d[sz/2];
-            if (sz/2) {
-                l = new RBSNode(d[0..sz/2]);
-            }
-            if (sz-sz/2-1) {
-                r = new RBSNode(d[sz/2+1..sz]);
-            }
+        this(Node _l, Node _r, in T _v) {
+            value = _v; l = _l; r = _r;
+            sz = 1 + l.sz + r.sz;
+        }
+
+        this(Node _l, Node _r, int _sz, in T _v) {
+            value = _v; l = _l; _sz = sz; r = _r;
         }
         
         T get(int k) {
-            if (size(l) == k) return value;
-            if (k < size(l)) {
+            if (l.sz == k) return value;
+            if (k < l.sz) {
                 return l.get(k);
             } else {
-                return r.get(k-size(l)-1);
+                return r.get(k-l.sz-1);
             }
         }
 
-        void get(T[] d) {
-            if (l !is null) l.get(d[0..size(l)]);
-            d[size(l)] = value;
-            if (r !is null) r.get(d[size(l)+1..$]);
+        void get(T[] d, int cl, int cr) {
+            push(&this);
+            if (cl < l.sz) {
+                l.get(d, cl, min(cr, l.sz));
+                if (cr <= l.sz) return;
+                d = d[l.sz-cl..$];
+            }
+            cl -= l.sz; cr -= l.sz;
+            if (cl <= 0) {
+                d[0] = value;
+                d = d[1..$];
+            }
+            cl -= 1; cr -= 1;
+            if (0 < cr) r.get(d, max(0, cl), cr);
         }
 
-        T[] getAll() {
-            auto res = new T[](sz);
-            get(res);
-            return res;
+        void get(T[] d) {
+            push(&this);
+            if (l.sz) l.get(d[0..l.sz]);
+            d[l.sz] = value;
+            if (r.sz) r.get(d[l.sz+1..$]);
         }
     }
 
 
     Node mt;
-    int sz;
+
+    @property int size() {
+        return mt.sz;
+    }
 
     this(Node d) {
         mt = d;
-        sz = size(mt);
     }
 
     this(T[] d) {
-        mt = new RBSNode(d);
-        sz = cast(int)d.length;
+        mt = built(d);
     }
 
-    RBSTree split(int k) {
+
+    RBSTree split(in int k) {
         auto y = split(mt, k);
         mt = y[0];
-        sz = size(mt);
-        return new RBSTree(y[1]);
+        return RBSTree(y[1]);
     }
 
-    RBSTree[] split(in int[] k) {
-        auto res = new RBSTree[](k.length+1);
+    void split(RBSTree[] res, in int[] k) {
         foreach_reverse(i, d; k) {
             res[i+1] = split(d);
         }
         res[0] = this;
-        return res;
     }
 
     RBSTree merge(RBSTree r) {
         mt = merge(mt, r.mt);
-        sz = size(mt);
         return this;
+    }
+
+    void reverse(int l, int r) {
+        __gshared RBSTree[3] x;
+        split(x, [l, r]);
+        x[1].mt.rev ^= true;
+        mt = x[0].merge(x[1]).merge(x[2]).mt;
     }
 
     void insert(int k, T d) {
         auto y = split(mt, k);
         mt = merge(merge(y[0], new RBSNode(d)), y[1]);
-        sz++;
     }
 
     T get(int k) {
         return mt.get(k);
+    }
+
+    T[] get(T[] res, int l, int r) {
+        mt.get(res, l, r);
+        return res;
     }
 
     T[] getAll() {
-        return mt.getAll();
-    }
-}
-
-
-class DRBSTree(T) {
-    RBSTree!T mt, rt;
-    int sz;
-    this() {}
-    this(RBSTree!T m, RBSTree!T r) {
-        assert(m.sz == r.sz);
-        mt = m; rt = r;
-        sz = m.sz;
-    }
-
-    this(T[] d) {
-        mt = new RBSTree!T(d);
-        rt = new RBSTree!T(d.dup.reverse);
-        sz = cast(int)d.length;
-    }
-
-    DRBSTree merge(DRBSTree r) {
-        mt = mt.merge(r.mt);
-        rt = r.rt.merge(rt);
-        sz = mt.sz;
-        return this;
-    }
-
-    DRBSTree[] split(in int[] k) {
-        int n = cast(int)k.length;
-        auto res = new DRBSTree[](n+1);
-        foreach (int i; 0..n+1) {
-            res[i] = new DRBSTree();
-        }
-        foreach_reverse (int i; 0..n) {
-            res[i+1].mt = mt.split(k[i]);
-            res[n-1-i].rt = rt.split(sz-k[n-1-i]);
-        }
-        res[0].mt = mt;
-        res[n].rt = rt;
-        res[0].sz = k[0];
-        foreach (int i; 1..n) {
-            res[i].sz = k[i]-k[i-1];
-        }
-        res[n].sz = sz-k[n-1];
-        return res;
-    }
-
-    void reverse(int l, int r) {
-        auto x = mt.split([l, r]);
-        auto y = rt.split([sz-r, sz-l]);
-        mt = x[0].merge(y[1]).merge(x[2]);
-        rt = y[0].merge(x[1]).merge(y[2]);
-    }
-
-    T get(int k) {
-        return mt.get(k);
-    }
-
-    T[][2] getAll() {
-        T[][2] res;
-        res[0] = mt.getAll;
-        res[1] = rt.getAll;
-        return res;
+        T[] res = new T[](mt.sz);
+        return get(res, 0, mt.sz);
     }
 }
--- Dlang/Geo/circle.d
+++ Dlang/Geo/circle.d
@@ -9,54 +9,44 @@ Cir curcumCircle(P a, P b, P c) {
 }
 
 Cir smallestEnclosingCircle(P[] p) {
+    P[10000] pbc;
+    P[3] q;
     const eps = 1e-9;
-    Cir dfs(P[] p, int pc, P[] q, int qc) {
-        Cir get3(in P[3] q) {
-            Cir c;
-            foreach (int i; 0..3) {
-                c[0] = (q[i]+q[(i+1)%3])/2;
-                c[1] = abs(q[i]-q[(i+1)%3])/2;
-                if (abs(q[(i+2)%3]-c[0]) <= c[1] + eps) {
-                    return c;
-                }
-            }
-            return curcumCircle(q[0], q[1], q[2]);            
-        }
-        Cir get(P[] q, int qc) {
-            if (qc == 0) {
+    int pbc = 1;
+    int cnt = 0;
+    Cir dfs(int pc, int qc) {
+        Cir get() {
+            final switch (qc) {
+            case 3:
+                return curcumCircle(q[0], q[1], q[2]);
+            case 2:
+                return Cir((q[0]+q[1])/2, abs(q[0]-q[1])/2);
+            case 1:
+                return Cir(q[0], 0);
+            case 0:
                 return Cir(P(0, 0), -1);
             }
-            if (qc == 1) {
-                return Cir(q[0], 0);
-            }
-            if (qc == 2) {
-                return Cir((q[0]+q[1])/2, abs(q[0]-q[1])/2);
-            }
-            Cir c, r;
-            c[1] = -1;
-            foreach (i; 0..qc-1) {
-                foreach (j; i+1..qc-1) {
-                    r = get3([q[i], q[j], q[qc-1]]);
-                    if (c[1] < r[1]) {
-                        c = r;
-                    }
-                }
-            }
-            return c;
         }
-        Cir c = get(q, qc);
+        Cir c = get();
+        foreach_reverse (i; max(-pc+1, 1)..pbc) {
+            if ( abs(pb[i]-c[0]) <= c[1] + eps) {
+                continue;
+            }
+            q[qc++] = pb[i];
+            c = dfs(-i, qc);
+            qc--;
+        }
         foreach (int i; 0..pc) {
-            if (abs(p[i]-c[0]) <= c[1] + eps) {
+            if (p[i].re == p[i].re.infinity || abs(p[i]-c[0]) <= c[1] + eps) {
                 continue;
             }
             q[qc++] = p[i];
-            c = dfs(p, i, q, qc);
+            c = dfs(i, qc);
             qc--;
+            pb[pbc++] = p[i];
+            p[i].re = double.infinity;
         }
         return c;
     }
-    static Xorshift rnd = Xorshift(252521);
-    randomShuffle(p, rnd);
-    static P[100] q;
-    return dfs(p, cast(int)p.length, q, 0);
+    return dfs(cast(int)p.length, 0);
 }
