19fd8a0426c801c8532ebca2590e1a5d75db4a79
Morita Kohei <yosupo06@gmail.com>
2015-02-26 16:56:32 +0900

    Add GroupCount to Dlang's UnionFind

--- Dlang/DataStruct/unionfind.d
+++ Dlang/DataStruct/unionfind.d
@@ -1,8 +1,18 @@
+/**
+ * Quick Find Weighted
+ * merge:O(logN)
+ * isSame:O(1)
+ * numGroup:O(1)
+ */
+
 class UnionFind {
 	int[] i2g;
 	int[][] g2i;
+	int gn;
 	this(int n) {
-		i2g.length = g2i.length = n;
+		i2g = new int[](n);
+		g2i = new int[][](n);
+		gn = n;
 		foreach (i; 0..n) {
 			i2g[i] = i;
 			g2i[i] = [i];
@@ -11,6 +21,7 @@ class UnionFind {
 
 	void merge(int a, int b) {
 		if (isSame(a, b)) return;
+		gn--;
 		int ga = i2g[a], gb = i2g[b];
 		if (g2i[ga].length > g2i[gb].length) swap(ga, gb);
 		foreach (i; g2i[ga]) i2g[i] = gb;
@@ -20,5 +31,9 @@ class UnionFind {
 
 	bool isSame(int a, int b) {
 		return i2g[a] == i2g[b];
+	}
+
+	int numGroup() {
+		return gn;
 	}
 }
