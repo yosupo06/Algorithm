bdc47dcbe43481d2292a79e79eb821ad5588c4e1
Morita Kohei <yosupo06@gmail.com>
2015-02-26 17:19:15 +0900

    add isprime

--- Dlang/Math/primes.d
+++ Dlang/Math/primes.d
@@ -1,3 +1,13 @@
+//O(sqrt(x))
+bool isPrime(int x) {
+    if (x <= 1) return false;
+    for (int i = 2; i*i <= x; i++) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+
 class Primes {
     import std.algorithm : map;
     import std.array : array;
