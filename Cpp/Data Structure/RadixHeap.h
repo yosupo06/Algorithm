e57f4c51601bef20ad4f7fae336e40039e6b8177
yosupo <yosupo06@gmail.com>
2015-04-03 22:21:51 +0900

    RadixHeapの修正

--- Cpp/Data Structure/RadixHeap.h
+++ Cpp/Data Structure/RadixHeap.h
@@ -1,37 +1,42 @@
-/*
-RadixHeap
-整数距離dijkstraで使うことを想定
-priority_queueに比べ高速
-*/
+int bsr(uint x) {
+    if (x == 0) return -1;
+    return 31-__builtin_clz(x);
+}
 
 struct RadixHeap {
-    typedef pair<int, int> P;
-    vector<P> v[32];
-    int mi, sz;
-    RadixHeap(int x, int p): sz(1) {
-        mi = x;
-        v[0].push_back(P(x, p));
+    typedef pair<uint, int> P;
+    vector<P> v[33];
+    uint last, sz;
+    RadixHeap() {
+        last = sz = 0;
     }
-    void push(int x, int p) {
+    void push(uint x, int p) {
+        assert(last <= x);
         sz++;
-        v[nlz(x^mi)+1].push_back(P(x, p));
+        v[bsr(x^last)+1].push_back(P(x, p));
     }
-    P pop() {
-        sz--;
+    void push(P p) {
+        push(p.first, p.second);
+    }
+    P top() {
+        return pop(false);
+    }
+    P pop(bool f = true) {
+        assert(sz);
         if (!v[0].size()) {
             int i = 1;
             while (!v[i].size()) i++;
-            mi = v[i][0].first;
-            for (P &d: v[i]) {
-                mi = min(mi, d.first);
-            }
-            for (P &p: v[i]) {
-                v[nlz(mi^p.first)+1].push_back(p);
+            last = min_element(v[i].begin(), v[i].end())->first;
+            for (P p: v[i]) {
+                v[bsr(p.first^last)+1].push_back(p);
             }
             v[i].clear();
         }
         P r = v[0].back();
-        v[0].pop_back();
+        if (f) {
+            sz--;
+            v[0].pop_back();
+        }
         return r;
     }
     int size() {
@@ -39,5 +44,39 @@ struct RadixHeap {
     }
     bool empty() {
         return sz == 0;
+    }
+    void clear() {
+        last = sz = 0;
+        for (int i = 0; i < 33; i++) {
+            v[i].clear();
+        }
+    }
+};
+
+struct RadixHeapInt {
+    vector<uint> v[33];
+    uint last, sz;
+    RadixHeap() {
+        last = sz = 0;
+    }
+    void push(uint x) {
+        assert(last <= x);
+        sz++;
+        v[bsr(x^last)+1].push_back(P(x, p));
+    }
+    P pop() {
+        assert(sz);
+        if (!v[0].size()) {
+            int i = 1;
+            while (!v[i].size()) i++;
+            last = *min_element(v[i].begin(), v[i].end());
+            for (uint x: v[i]) {
+                v[bsr(x^last)+1].push_back(x);
+            }
+            v[i].clear();
+        }
+        sz--;
+        v[0].pop_back();
+        return last;
     }
 };
