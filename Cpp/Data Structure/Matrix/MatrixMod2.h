6e4c31964b068677ccb8d02e5cd8b688eabe8feb
yosupo <yosupo06@gmail.com>
2015-06-17 21:54:04 +0900

    F2用の行列を追加

---
+++ Cpp/Data Structure/Matrix/MatrixMod2.h
@@ -0,0 +1,65 @@
+/**
+ * 行列ライブラリ with Mod2
+ */
+template<int N, int M>
+struct MatrixMod2 {
+    array<bitset<M>, N> d;
+
+    MatrixMod2() {
+        for (int i = 0; i < N; i++) {
+            d[i].reset();
+        }
+    }
+
+    bitset<M>& operator[](int p) {
+        return d[p];
+    }
+    
+    const bitset<M>& operator[](int p) const {
+        return d[p];
+    }
+    
+    MatrixMod2& operator=(const MatrixMod2 &other) {
+        d = other.d;
+        return *this;
+    }
+
+    MatrixMod2 operator+(const MatrixMod2 &right) {
+        MatrixMod2 res(N, M);
+        for (int i = 0; i < N; i++) {
+            res[i] = d[i]^right[i];
+        }
+        return res;
+    }
+    
+    template<int K>
+    MatrixMod2<N, K> operator*(const MatrixMod2<M, K> &right) {
+        MatrixMod2<N, K> res;
+        MatrixMod2<K, M> r;
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < M; j++) {
+                r[i][j] = right[j][i]; 
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                res[i][j] = (d[i]&r[j]).count() % 2;
+            }
+        }
+        return res;
+    }
+
+    MatrixMod2 pow(ll p) {
+        assert(N == M);
+        MatrixMod2<N, M> res, buf = *this;
+        for (int i = 0; i < N; i++) res[i][i] = true;
+        while(p != 0) {
+            if (p % 2) {
+                res = res*buf;
+            }
+            p /= 2;
+            buf = buf*buf;
+        }
+        return res;
+    }
+};
