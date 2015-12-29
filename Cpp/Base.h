e7c944a74303fc92c5f026ac5ed683bccfc9ae47
森田晃平 <kohei@ysp-mac.local>
2015-12-29 16:46:58 +0900

    change Base.h

--- Cpp/Base.h
+++ Cpp/Base.h
@@ -1,5 +1,5 @@
 /**
- * Ubuntuコマンド
+ * Ubuntu
  * gsettings set org.gnome.desktop.interface gtk-key-theme Emacs
  * setxkbmap -option ctrl:nocaps
  **/
@@ -13,7 +13,7 @@ ll lcm(ll a, ll b) {
     return a/gcd(a, b)*b;
 }
 
-/// 高速累乗, x^nをO(logn)で求める
+/// x^n
 ll pow(ll x, ll n) {
     ll r = 1;
     while (n) {
@@ -26,30 +26,21 @@ ll pow(ll x, ll n) {
 
 /// x^n % md
 ll pow_mod(ll x, ll n, ll md) {
-	ll r = 1;
-	while (n) {
-		if (n & 1) r = (r * x) % md;
-		x = (x * x) % md;
-		n >>= 1;
-	}
-	return r;
+    ll r = 1;
+    while (n) {
+        if (n & 1) r = (r * x) % md;
+        x = (x * x) % md;
+        n >>= 1;
+    }
+    return r;
 }
 
-/// xの逆元, mdは素数かつxはmdの倍数ではない必要がある
+/// xの逆元, xとmdは互いに素
 ll invp(ll x, ll md) {
-	return pow_mod(x, md-2, md);
+    return pow_mod(x, md-2, md);
 }
 
-int bsr(uint n, int p = 31) {
-    return (p == -1 || n & (1<<p)) ? p : static_bsr(n, p-1);
-}
 
-int static_ceil_log2(uint n) {
-    return n == 0 ? -1 : 1+static_bsr(n-1);
-}
-
-//時間計測は闇が多いことに注意
-//codeforcesで動作確認
 namespace StopWatch {
     clock_t st;
     bool f = false;
@@ -63,12 +54,12 @@ namespace StopWatch {
     }
 }
 
-/*AOJ用 stack sizeを拡張する*/
+/*stack sizeを拡張する*/
 int main() {
     static ll eord, enew;
     const int sz = 256*1024*1024;
     static void *p = malloc(sz);
-    enew = (long long)p + sz - 16;
+    enew = (ll)((p + sz) & ~0xff);
     __asm__ volatile("mov %%rsp, %0" : "=r"(eord));
     __asm__ volatile("mov %0, %%rsp" : : "r"(enew));
     main2();
