89deb9cc9fb3cd7d2724b02a255fe4621518cdd7
yosupo <yosupo06@gmail.com>
2016-01-25 21:37:12 +0900

    リファクタリング

--- Cpp/Graph/SCC.h
+++ Cpp/Graph/SCC.h
@@ -1,9 +1,5 @@
 /**
- * SCC, つまり強連結成分分解を行うライブラリ
- *
- * 使用法は結構複雑
- *
- * template引数のint Vは頂点の最大数
+ * SCC
  */
 template<int V>
 struct SCC {
@@ -19,8 +15,7 @@ struct SCC {
     void dfs(int v) {
         used[v] = true;
         for (int d: g[v]) {
-            if (used[d]) continue;
-            dfs(d);
+            if (!used[d]) dfs(d);
         }
         vs.push_back(v);
     }
@@ -28,32 +23,21 @@ struct SCC {
     vector<int> scc[V]; /// scc[i] = i個目の強連結成分に属する頂点
     void rdfs(int v, int k) {
         used[v] = true;
-        res[v] = k;
-        scc[k].push_back(v);
+        res[v] = k; scc[k].push_back(v);
         for (int d: rg[v]) {
-            if (used[d]) continue;
-            rdfs(d, k);
+            if (!used[d]) rdfs(d, k);
         }
     }
 
-    /**
-     * 頂点0..v-1に対してSCCを行う
-     *
-     * 返り値として強連結成分の個数が返される
-     * これを呼び出すとres[0..v-1], scc[0..v-1]に情報が入る
-     */
     int exec(int v) {
         fill_n(used, v, false);
         for (int i = 0; i < v; i++) {
-            if (used[i]) continue;
-            dfs(i);
+            if (!used[i]) dfs(i);
         }
         fill_n(used, v, false);
         int k = 0;
         for (int i = vs.size()-1; i >= 0; i--) {
-            if (used[vs[i]]) continue;
-            rdfs(vs[i], k);
-            k++;
+            if (!used[vs[i]]) rdfs(vs[i], k++);
         }
         return k;
     }
