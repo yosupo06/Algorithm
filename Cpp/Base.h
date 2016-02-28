f0bb26af9eac45be438f33f1671772b95ecb20ae
yosupo <yosupo06@gmail.com>
2016-02-29 00:56:00 +0900

    ModIntを追加

--- Cpp/Base.h
+++ Cpp/Base.h
@@ -14,8 +14,9 @@ ll lcm(ll a, ll b) {
 }
 
 /// x^n
-ll pow(ll x, ll n) {
-    ll r = 1;
+template<class T>
+T pow(T x, ll n) {
+    T r = 1;
     while (n) {
         if (n & 1) r *= x;
         x *= x;
@@ -45,3 +46,21 @@ ll invp(ll x, ll md) {
 int bsr(int x) {
     return 31 - __builtin_clz(x);
 }
+
+template<uint MD>
+struct ModInt {
+    uint v;
+    ModInt() : v(0) {}
+    ModInt(ll v) : v((v%MD+MD)%MD) {}
+    ModInt normS(uint x) {return (x<MD)?x:x-MD;};
+    ModInt operator+(ModInt r) {return ModInt(normS(v+r.v));};
+    ModInt operator-(ModInt r) {return *this+ModInt(MD-r.v);};
+    ModInt operator*(ModInt r) {return ModInt((ll)v*r.v%MD);};
+    ModInt operator+=(ModInt r) {return *this=*this+r;}
+    ModInt operator-=(ModInt r) {return *this=*this-r;}
+    ModInt operator*=(ModInt r) {return *this=*this*r;}
+    static ModInt inv(ModInt x) {
+        return pow(ModInt(x), MD-2);
+    }
+};
+
