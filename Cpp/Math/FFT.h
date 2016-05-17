1fe54c9c6450796f15eb1ab0dab09918adb3f713
yosupo <yosupo06@gmail.com>
2016-05-18 01:28:33 +0900

    FFTの精度が悪かった問題を改善

--- Cpp/Math/FFT.h
+++ Cpp/Math/FFT.h
@@ -1,5 +1,5 @@
 using R = double;
-const R PI = 4.0*atan(1.0);
+const R PI = 4*atan(R(1));
 using Pc = complex<R>;
 
 void fft(int s, bool type, Pc c[]) {
@@ -8,19 +8,17 @@ void fft(int s, bool type, Pc c[]) {
     Pc *b = new Pc[N];
     copy_n(c, N, a);
     for (int i = 1; i <= s; i++) {
-        int H = 1<<(i-1), W = 1<<(s-i); //高さ2H, 幅W
-        Pc base = polar<R>(1, (type ? -PI : PI) / H);
-        Pc now = Pc(1, 0);
-        for (int y = 0; y < (N>>1); y += W) {
+        int W = 1<<(s-i); //変更後の幅W
+        for (int y = 0; y < N/2; y += W) {
+            Pc now = polar<R>(1, y*(type ? -2*PI : 2*PI)/N);
             for (int x = 0; x < W; x++) {
                 auto l =       a[y<<1 | x];
                 auto r = now * a[y<<1 | x | W];
                 b[y | x]        = l+r;
                 b[y | x | N>>1] = l-r;
             }
-            now *= base;
         }
-        swap(a, b);
+        swap(a, b);            
     }
     copy_n(a, N, c);
     delete[] a;
