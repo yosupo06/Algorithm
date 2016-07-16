2d6ba5fe244aa036dbb3252b37a3775d93d63e2e
yosupo <yosupo06@gmail.com>
2016-07-16 19:51:13 +0900

    change SA-IS's format & SuffixArray's bug fix

--- Cpp/String/SAIS.h
+++ Cpp/String/SAIS.h
@@ -1,33 +1,16 @@
-/**
- * Suffix Array
- */
+template<class Str>
+struct SA {
+    Str s;
+    vector<int> sa, rsa, lcp;
 
-namespace SuffixArray {
-    template<class T>
-    void induce(int n, const T s[], bool ls[], int sa[], int l_basm[], int B) {
-        int buf[B+10];
-        copy_n(l_basm, B+1, buf);
-        for (int i = 0; i <= n; i++) {
-            if (sa[i] >= 1 && !ls[sa[i]-1]) {
-                int bc = s[sa[i]-1];
-                sa[buf[bc]++] = sa[i]-1;
-            }
-        }
+    vector<bool> ls;
+    SA(Str s, int B = 200) : s(s) {
+        int n = (int)s.size();
+        sa.resize(n+1); rsa.resize(n+1); lcp.clear();
+        ls.resize(n+1); //L:false S:true
 
-        copy_n(l_basm, B+1, buf);
-        for (int i = n; i > 0; i--) {
-            if (sa[i] >= 1 && ls[sa[i]-1]) {
-                int bc = s[sa[i]-1];
-                sa[--buf[bc+1]] = sa[i]-1;
-            }
-        }
-    }
-
-    template<class T>
-    void SA(int n, const T s[], int sa[], int B = 200) {
-        bool ls[n+1]; //L:false S:true
-        int l_ba[B+1], s_ba[B+1];
-        fill_n(l_ba, B+1, 0); fill_n(s_ba, B+1, 0);
+        vector<int> l_ba(B), s_ba(B), l_basm(B+1), s_basm(B+1);
+        fill_n(l_ba.begin(), B, 0); fill_n(s_ba.begin(), B, 0);
         ls[n] = true;
         for (int i = n-1; i >= 0; i--) {
             if (i == n-1 || s[i] > s[i+1]) {
@@ -38,10 +21,16 @@ namespace SuffixArray {
                 ls[i] = ls[i+1];
             }
             if (!ls[i]) {
-                l_ba[(int)s[i]]++;
+                l_ba[s[i]]++;
             } else {
-                s_ba[(int)s[i]]++;
+                s_ba[s[i]]++;
             }
+        }
+
+        l_basm[0] = 1; s_basm[0] = 1+l_ba[0];
+        for (int i = 1; i <= B; i++) {
+            l_basm[i] = s_basm[i-1]+s_ba[i-1];
+            s_basm[i] = l_basm[i]+l_ba[i];
         }
 
         vector<int> v;
@@ -54,33 +43,25 @@ namespace SuffixArray {
                 v.push_back(i);
             }
         }
-        
-        int l_basm[B+1], s_basm[B+1];
-        l_basm[0] = 1; s_basm[0] = 1+l_ba[0];
-        for (int i = 1; i <= B; i++) {
-            l_basm[i] = s_basm[i-1]+s_ba[i-1];
-            s_basm[i] = l_basm[i]+l_ba[i];
-        }
 
         if (v.size()) {
             vector<int> v2 = v;
             int buf[B+1];
-            copy_n(s_basm, B+1, buf);
-            fill_n(sa, n+1, -1);
+            copy_n(s_basm.begin(), B+1, buf);
+            fill_n(sa.begin(), n+1, -1);
             sa[0] = n;
             for (int i = 0; i < (int)v.size(); i++) {
                 int c = s[v[i]];
                 sa[buf[c]++] = v[i];
             }
-            induce(n, s, ls, sa, l_basm, B);
+            induce(l_basm);
             int c = 0;
             for (int i = 1; i <= n; i++) {
                 if (lms[sa[i]] == -1) continue;
                 v[c++] = sa[i];
             }
 
-            int s2[v.size()];
-            int sa2[v.size()+1];
+            vector<int> s2(v.size());
             c = 0;
             s2[lms[v[0]]] = 0;
             for (int i = 1; i < (int)v.size(); i++) {
@@ -100,40 +81,45 @@ namespace SuffixArray {
                 }
                 s2[lms[v[i]]] = c;
             }
-            SA(v.size(), s2, sa2, c);
+            SA<vector<int>> sa_child(s2, c+1);
             for (int i = 1; i <= (int)v.size(); i++) {
-                v[i-1] = v2[sa2[i]];
+                v[i-1] = v2[sa_child.sa[i]];
             }
         }
 
         int buf[B+1];
-        copy_n(s_basm, B+1, buf);
+        copy_n(s_basm.begin(), B+1, buf);
 
-        fill_n(sa, n+1, -1);
+        fill_n(sa.begin(), n+1, -1);
         sa[0] = n;
         for (int i = 0; i < (int)v.size(); i++) {
             int c = s[v[i]];
             sa[buf[c]++] = v[i];
         }
 
-        induce(n, s, ls, sa, l_basm, B);
-    }
+        induce(l_basm);
 
-    template <class T>
-    void LCP(int n, const T s[], const int sa[], int lcp[]) {
-        int rsa[n+1];
         for (int i = 0; i <= n; i++) {
             rsa[sa[i]] = i;
         }
-        int h = 0;
-        for (int i = 0; i < n; i++) {
-            int j = sa[rsa[i]-1];
+    }
 
-            if (h > 0) h--;
-            for (; j+h < n && i+h < n; h++) {
-                if (s[j+h] != s[i+h]) break;
+    void induce(vector<int> &lba_sm) {
+        int n = (int)s.size();
+        vector<int> buf;
+        buf = lba_sm;
+        for (int i = 0; i <= n; i++) {
+            if (sa[i] >= 1 && !ls[sa[i]-1]) {
+                int bc = s[sa[i]-1];
+                sa[buf[bc]++] = sa[i]-1;
             }
-            lcp[rsa[i]-1] = h;
+        }
+        buf = lba_sm;
+        for (int i = n; i > 0; i--) {
+            if (sa[i] >= 1 && ls[sa[i]-1]) {
+                int bc = s[sa[i]-1];
+                sa[--buf[bc+1]] = sa[i]-1;
+            }
         }
     }
-}
+};
--- Cpp/String/SuffixArray.h
+++ Cpp/String/SuffixArray.h
@@ -1,30 +1,28 @@
-template<int N>
+template<class Str>
 struct SA {
-	static int n, k;
-	static int rank[N+1], tmp[N+1];
-
-	static bool com(int i, int j) {
-		if (rank[i] != rank[j]) return rank[i] < rank[j];
-		int ri = i+k <= n ? rank[i+k] : -1;
-		int rj = j+k <= n ? rank[j+k] : -1;
-		return ri < rj;
-	}
-
-	void con(string S, int *sa) {
-		int n = (int)S.size();
-		for (int i = 0; i <= n; i++) {
-			sa[i] = i;
-			rank[i] = i < n ? S[i] : -1;
-		}
-		for (k = 1; k <= n; k *= 2) {
-			sort(sa, sa+n+1, com);
-			tmp[sa[0]] = 0;
-			for (int i = 1; i <= n; i++) {
-				tmp[sa[i]] = tmp[sa[i-1]] + (com(sa[i-1], sa[i]) ? 1 : 0);
-			}
-			for (int i = 0; i <= n; i++) {
-				rank[i] = tmp[i];
-			}
-		}
-	}
+    Str s;
+    vector<int> sa, rsa, lcp;
+    SA(Str s) : s(s) {
+        int n = (int)s.size();
+        sa.resize(n+1); rsa.resize(n+1); lcp.clear();
+        iota(sa.begin(), sa.end(), 0);
+        for (int i = 0; i <= n; i++) {
+            rsa[i] = i < n ? s[i] : -1;
+        }
+        vector<int> tmp(n+1);
+        for (int k = 1; k <= n; k *= 2) {
+            auto cmp = [&](int x, int y){
+                if (rsa[x] != rsa[y]) return rsa[x] < rsa[y];
+                int rx = x+k <= n ? rsa[x+k] : -1;
+                int ry = y+k <= n ? rsa[y+k] : -1;
+                return rx < ry;
+            };
+            sort(sa.begin(), sa.end(), cmp);
+            tmp[sa[0]] = 0;
+            for (int i = 1; i <= n; i++) {
+                tmp[sa[i]] = tmp[sa[i-1]] + (cmp(sa[i-1], sa[i]) ? 1 : 0);
+            }
+            copy_n(tmp.begin(), n+1, rsa.begin());
+        }
+    }
 };
