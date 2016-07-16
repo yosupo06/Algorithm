2dcda82c0148768ead662f79cd073d740ccc5fc0
yosupo <yosupo06@gmail.com>
2016-07-16 19:50:57 +0900

    manacher's bug fix

--- Cpp/String/Manacher.h
+++ Cpp/String/Manacher.h
@@ -1,9 +1,9 @@
 //!!!not verify!!!
 vector<int> manacher(string s) {
-    vector<int> R(s.size()+1);
     int n = (int)s.size();
+    vector<int> R(n+1); R[0] = 0;
     for (int i = 0, j = 0; i < n; i++) {
-        int &k = R[i]; k = min(j+R[j]-i, R[2*j-i]);
+        int &k = R[i]; k = (j + R[j] <= i) ? 0 : min(j+R[j]-i, R[2*j-i]);
         while (0 <= i-k and i+k < n and s[i-k] == s[i+k]) k++;
         if (j + R[j] < i + R[i]) j = i;
     }
