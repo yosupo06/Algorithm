d802f2b4ee3204e3b902aa33bf9688a9cdbffa42
森田晃平 <yosupo@yosupo.local>
2016-10-11 19:12:39 +0900

    add Starry

--- Cpp/DataStructure/StarrySkyTree.h
+++ Cpp/DataStructure/StarrySkyTree.h
@@ -1,17 +1,38 @@
 template<class N>
 struct SegTree {
-    int sz;
+    int lg, sz;
     vector<N> n;
     SegTree(int sz) {
-        sz = 1<<bsr(2*sz-1);
+        lg = bsr(2*sz-1);
+        sz = 1<<lg;
         this->sz = sz;
         n = vector<N>(2*sz);
+        for (int i = 2*sz-1; i >= sz; i--) {
+            n[i] = N();
+        }
         for (int i = sz-1; i >= 1; i--) {
             n[i] = N(n[2*i], n[2*i+1]);
         }
     }
     template<class Q>
-    void query(int a, int b, Q &&q, int k, int sz) {
+    Q single(int idx, Q q) {
+        if (idx < 0 or sz <= idx) return q;
+        idx += sz;
+        for (int i = lg; i >= 1; i--) {
+            int k = idx>>i;
+            n[k].push(n[2*k], n[2*k+1]);
+        }
+        q += n[idx];
+        if (q.update()) {
+            for (int i = 1; i <= lg; i++) {
+                int k = idx>>i;
+                n[k].update(n[2*k], n[2*k+1]);
+            }
+        }
+        return q;
+    }
+    template<class Q>
+    void query(int a, int b, Q &q, int k, int sz) {
         if (a <= 0 and sz <= b) {
             q += n[k];
             return;
@@ -22,7 +43,7 @@ struct SegTree {
         if (q.update()) n[k].update(n[2*k], n[2*k+1]);
     }
     template<class Q>
-    Q query(int a, int b, Q &&q) {
+    Q query(int a, int b, Q q) {
         if (a < sz and 0 < b) query(a, b, q, 1, sz);
         return q;
     }
@@ -30,8 +51,11 @@ struct SegTree {
 
 struct Node {
     ll ma, lz;
-    Node() : ma(0), lz(0) {}
-    Node(Node &l, Node &r) { update(l, r); }
+    void init() {
+        ma = lz = 0;
+    }
+    Node() { init(); }
+    Node(Node &l, Node &r) { init(); update(l, r); }
     void update(const Node &l, const Node &r) {
         ma = max(l.ma, r.ma);
     }
@@ -45,16 +69,15 @@ struct Node {
     void lzdata(ll x) {
         ma += x; lz += x;
     }
-};
-
-struct MaxQuery {
-    ll ma = -TEN(18);
-    static constexpr bool update() { return false; }
-    void operator+=(Node &r) { ma = max(ma, r.ma); }
-};
-
-struct AddQuery {
-    ll x;
-    static constexpr bool update() { return true; }
-    void operator+=(Node &n) { n.lzdata(x); }
+    // query
+    struct MaxQuery {
+        ll ma = -TEN(18);
+        static constexpr bool update() { return false; }
+        void operator+=(Node &r) { ma = max(ma, r.ma); }
+    };
+    struct AddQuery {
+        ll x;
+        static constexpr bool update() { return true; }
+        void operator+=(Node &n) { n.lzdata(x); }
+    };
 };
