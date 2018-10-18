53cc86335aab77102fc05583b93c439a938ab199
Kohei Morita <yosupo06@gmail.com>
2018-10-18 13:35:35 +0900

    separete fft_test

--- test/math/fft_test.cpp
+++ test/math/fft_test.cpp
@@ -1,67 +1,36 @@
 #include "gtest/gtest.h"
+#include "algotest/math/fft_test.h"
+
+using namespace algotest;
+
 #include "base.h"
-#include "random.h"
 #include "math/modint.h"
 #include "fft.h"
 
-TEST(FFTYosupo, FFTRandom) {
-    auto naive_fft = [&](V<int> a, V<int> b) {
-        int A = int(a.size()), B = int(b.size());
-        V<int> c(A+B-1);
-        for (int i = 0; i < A; i++) {
-            for (int j = 0; j < B; j++) {
-                c[i+j] += a[i] * b[j];
-            }
-        }
-        return c;
-    };
 
-    auto fast_fft = [&](V<int> a, V<int> b) {
-        int A = int(a.size()), B = int(b.size());
-        V<D> na(A), nb(B);
-        for (int i = 0; i < A; i++) na[i] = a[i];
-        for (int i = 0; i < B; i++) nb[i] = b[i];
-        auto nc = multiply(na, nb);
-        V<int> c(A+B-1);
-        for (int i = 0; i < A+B-1; i++) c[i] = int(round(nc[i]));
-        return c;
-    };
-
-    auto fast2_fft = [&](V<int> a, V<int> b) {
-        int A = int(a.size()), B = int(b.size());
-        V<Pc> na(A), nb(B);
-        for (int i = 0; i < A; i++) na[i] = Pc(a[i], 0);
-        for (int i = 0; i < B; i++) nb[i] = Pc(b[i], 0);
-        auto nc = multiply(na, nb);
-        V<int> c(A+B-1);
-        for (int i = 0; i < A+B-1; i++) c[i] = int(round(nc[i].real()));
-        return c;
-    };
-
-    auto check_fft = [&](int A, int B) {
-        V<int> a(A), b(B);
-        for (int i = 0; i < A; i++) {
-            a[i] = rand_int(0, 100);
-        }
-        for (int i = 0; i < B; i++) {
-            b[i] = rand_int(0, 100);
-        }
-        V<int> c1 = naive_fft(a, b);
-        V<int> c2 = fast_fft(a, b);
-        V<int> c3 = fast2_fft(a, b);
-        ASSERT_EQ(c1.size(), A+B-1);
-        ASSERT_EQ(c2.size(), A+B-1);
-        ASSERT_EQ(c3.size(), A+B-1);
-        for (int i = 0; i < A+B-1; i++) {
-            assert(c1[i] == c2[i]);
-            assert(c1[i] == c3[i]);
-        }
-    };
-
-    int N = 50;
-    for (int l = 1; l < N; l++) {
-        for (int r = 1; r < N; r++) {
-            check_fft(l, r);
-        }
+struct DoubleFFTTester : public FFTTesterBase {
+    V<ll> multiply(V<ll> _a, V<ll> _b) final {
+        V<D> a, b;
+        for (auto d: _a) a.push_back(d);
+        for (auto d: _b) b.push_back(d);
+        auto c = ::multiply(a, b);
+        V<ll> _c;
+        for (auto d: c) _c.push_back(ll(round(d)));
+        return _c;
     }
-}
+};
+
+struct ComplexFFTTester : public FFTTesterBase {
+    V<ll> multiply(V<ll> _a, V<ll> _b) final {
+            V<Pc> a, b;
+            for (auto d: _a) a.push_back(Pc(d));
+            for (auto d: _b) b.push_back(Pc(d));
+            auto c = ::multiply(a, b);
+            V<ll> _c;
+            for (auto d: c) _c.push_back(ll(round(d.real())));
+            return _c;
+    }
+};
+
+using FFTTypes = ::testing::Types<DoubleFFTTester, ComplexFFTTester>;
+INSTANTIATE_TYPED_TEST_CASE_P(MyFFT, FFTTest, FFTTypes);
