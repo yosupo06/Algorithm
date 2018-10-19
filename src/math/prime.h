569af239e4de8242b6e26bfee19279260d5a0099
Kohei Morita <yosupo06@gmail.com>
2018-10-19 13:21:42 +0900

    fix prime

--- src/math/prime.h
+++ src/math/prime.h
@@ -39,7 +39,8 @@ bool is_prime(ll n) {
     if (n % 2 == 0) return false;
     ll d = n - 1;
     while (d % 2 == 0) d /= 2;
-    V<ll> alist{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};  // n < 2^64
+    // Michal Forisek, Jakub Jancina, Fast Primality Testing for Integers That Fit into a Machine Word
+    V<ll> alist{2, 325, 9375, 28178, 450775, 9780504, 1795265022LL};  // n < 2^64
     for (ll a : alist) {
         if (n <= a) break;
         ll t = d;
