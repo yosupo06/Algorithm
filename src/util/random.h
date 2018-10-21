502e3b205567c94e2ed79a516503bff80452e884
Kohei Morita <yosupo06@gmail.com>
2018-10-21 16:12:28 +0900

    fix random

--- src/util/random.h
+++ src/util/random.h
@@ -5,12 +5,6 @@ ll rand_int(ll l, ll r) { //[l, r]
     return D(l, r)(gen);
 }
 
-template<class URNG>
-ll rand_int(ll l, ll r, URNG &gen) { //[l, r]
-    using D = uniform_int_distribution<ll>;
-    return D(l, r)(gen);
-}
-
 double rand_double(double l, double r) { //[l, r]
     using D = uniform_real_distribution<double>;
     static random_device rd;
@@ -18,37 +12,10 @@ double rand_double(double l, double r) { //[l, r]
     return D(l, r)(gen);
 }
 
-template<class URNG>
-double rand_double(double l, double r, URNG &gen) { //[l, r]
-    using D = uniform_real_distribution<double>;
-    return D(l, r)(gen);
-}
-
-template<class URNG>
-ll rand_int_log(ll l, ll r, URNG &gen) {
-    assert(0 <= l);
-    long double a = log1p(l);
-    long double b = log1p(r);
-    while (true) {
-        long double c = uniform_real_distribution<long double>(a, b)(gen);
-        ll z = (ll)(round(expm1(c)));
-        if (l <= z && z <= r) return z;
-    }
-}
-
 string rand_lower_string(int n) {
     string s = "";
     for (int i = 0; i < n; i++) {
         s += 'a' + rand_int(0, 25);
-    }
-    return s;
-}
-
-template<class URNG>
-string rand_lower_string(int n, URNG &gen) {
-    string s = "";
-    for (int i = 0; i < n; i++) {
-        s += 'a' + rand_int(0, 25, gen);
     }
     return s;
 }
