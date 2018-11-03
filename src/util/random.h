19ef2a0aecec95a239dee2518d564f46d8320bf2
Kohei Morita <yosupo06@gmail.com>
2018-11-03 23:48:27 +0900

    separate scc_test

--- src/util/random.h
+++ src/util/random.h
@@ -5,22 +5,10 @@ ll rand_int(ll l, ll r) { //[l, r]
     return D(l, r)(gen);
 }
 
-template<class URNG>
-ll rand_int(ll l, ll r, URNG &gen) { //[l, r]
-    using D = uniform_int_distribution<ll>;
-    return D(l, r)(gen);
-}
-
 double rand_double(double l, double r) { //[l, r]
     using D = uniform_real_distribution<double>;
     static random_device rd;
     static mt19937 gen(rd());
-    return D(l, r)(gen);
-}
-
-template<class URNG>
-double rand_double(double l, double r, URNG &gen) { //[l, r]
-    using D = uniform_real_distribution<double>;
     return D(l, r)(gen);
 }
 
@@ -40,15 +28,6 @@ string rand_lower_string(int n) {
     string s = "";
     for (int i = 0; i < n; i++) {
         s += 'a' + rand_int(0, 25);
-    }
-    return s;
-}
-
-template<class URNG>
-string rand_lower_string(int n, URNG &gen) {
-    string s = "";
-    for (int i = 0; i < n; i++) {
-        s += 'a' + rand_int(0, 25, gen);
     }
     return s;
 }
