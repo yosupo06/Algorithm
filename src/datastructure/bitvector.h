7a3e3c8aec58793cf5c0f395ed88e874893c26a1
Kohei Morita <yosupo06@gmail.com>
2018-11-07 19:22:16 +0900

    fix bitvector

--- src/datastructure/bitvector.h
+++ src/datastructure/bitvector.h
@@ -1,33 +1,84 @@
 struct BitVec {
+    static constexpr size_t B = 64;
     size_t n;
     V<ull> d;
-    BitVec(size_t _n = 0) : n(_n), d((n+63)/64) {}
+    explicit BitVec(size_t _n = 0) : n(_n), d((n + B - 1) / B) {}
     size_t size() const { return n; }
-    bool operator[](size_t i) const { return (d[i/64] >> (i%64)) & 1; }
+    bool operator[](size_t i) const { return ((d[i / B] >> (i % B)) & 1) != 0; }
     void set(size_t i, bool f) {
-        if (f) d[i/64] |= (1ULL << (i%64));
-        else d[i/64] &= ~(1ULL << (i%64));
+        if (f)
+            d[i / B] |= (1ULL << (i % B));
+        else
+            d[i / B] &= ~(1ULL << (i % B));
     }
+    void reset() { fill(d.begin(), d.end(), 0); }
     void push_back(bool f) {
-        if (n % 64 == 0) d.push_back(0);
+        if (n % B == 0) d.push_back(0);
         set(n, f);
         n++;
     }
 
-    void swap_elms(int a, int b) { uint f = (*this)[a]; set(a, (*this)[b]); set(b, f); }
+    void swap_elms(size_t a, size_t b) {
+        bool f = (*this)[a];
+        set(a, (*this)[b]);
+        set(b, f);
+    }
 
-    template<class OP>
-    BitVec& op1(OP op) {
-        for (size_t i = 0; i < d.size(); i++) d[i] = op(d[i]);
+    template <class OP> BitVec& op1(OP op) {
+        for (auto& x : d) x = op(x);
         return *this;
     }
 
-    template<class OP>
-    BitVec& op2(const BitVec& r, OP op) {
+    template <class OP> BitVec& op2(const BitVec& r, OP op) {
         assert(n == r.n);
         for (size_t i = 0; i < d.size(); i++) d[i] = op(d[i], r.d[i]);
         return *this;
     }
+
+    BitVec& operator&=(const BitVec& r) { return op2(r, bit_and<ull>()); }
+    BitVec& operator|=(const BitVec& r) { return op2(r, bit_or<ull>()); }
     BitVec& operator^=(const BitVec& r) { return op2(r, bit_xor<ull>()); }
+    BitVec& operator<<=(const size_t& s) {
+        auto block = s / B, rem = s % B;
+        if (d.size() <= block) {
+            reset();
+            return *this;
+        }
+        for (size_t i = d.size() - 1; i > block; i--) {
+            if (rem == 0)
+                d[i] = d[i - block];
+            else {
+                d[i] = (d[i - block] << rem) | ((d[i - block - 1]) >> (B - rem));
+            }
+        }
+        d[block] = d[0] << rem;
+        fill(d.begin(), d.begin() + block, 0);
+        return *this;
+    }
+    BitVec& operator>>=(const size_t& s) {
+        //don't work
+        auto block = s / B, rem = s % B;
+        if (d.size() <= block) {
+            reset();
+            return *this;
+        }
+        for (size_t i = 0; i < d.size() - block; i++) {
+            if (rem == 0)
+                d[i] = d[i + block];
+            else {
+                ull u = (d[i + block] << rem);
+                if (i + block + 1 < d.size())
+                    u |= (d[i + block + 1]) >> (B - rem);
+                d[i] = u;
+            }
+        }
+        d[d.size() - block - 1] = (d[d.size() - 1] << rem);
+        for (size_t i = d.size() - block; i < d.size(); i++) d[i] = 0;
+        return *this;
+    }
+    BitVec operator&(const BitVec& r) const { return BitVec(*this) &= r; }
+    BitVec operator|(const BitVec& r) const { return BitVec(*this) |= r; }
     BitVec operator^(const BitVec& r) const { return BitVec(*this) ^= r; }
+    BitVec operator<<(const size_t& s) const { return BitVec(*this) <<= s; }
+    BitVec operator>>(const size_t& s) const { return BitVec(*this) >>= s; }
 };
