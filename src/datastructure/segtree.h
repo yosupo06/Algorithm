b934736e4941cfd90b40006d9b45274c32f20ea1
Kohei Morita <yosupo06@gmail.com>
2018-11-16 09:23:23 +0900

    add segtree

---
+++ src/datastructure/segtree.h
@@ -0,0 +1,58 @@
+
+template<class D, class L, class OPDD, class OPDL, class OPLL>
+struct SegTree {
+    D e_d;
+    L e_l;
+    OPDD opdd;
+    OPDL opdl;
+    OPLL opll;
+    int sz, lg; //(2^lgに拡張後の)サイズ, lg
+    V<D> d;
+    V<L> lz;
+
+    SegTree(V<D> first, D _e_d, L _e_l, OPDD _opdd, OPDL _opdl, OPLL _opll) : e_d(_e_d), e_l(_e_l), opdd(_opdd), opdl(_opdl), opll(_opll) {
+        int n = int(first.size());
+        lg = 1;
+        while ((1<<lg) < n) lg++;
+        sz = 1<<lg;
+        d = V<D>(2*sz, e_d);
+        lz = V<L>(2*sz, e_l);
+        for (int i = 0; i < n; i++) d[sz + i] = first[i];
+        for (int i = sz-1; i >= 0; i--) {
+            update(i);
+        }
+    }
+
+    void all_add(int k, L x) {
+        d[k] = opdl(d[k], x);
+        lz[k] = opll(lz[k], x);
+    }
+    void push(int k) {
+        all_add(2*k, lz[k]); all_add(2*k+1, lz[k]);
+        lz[k] = e_l;
+    }
+    void update(int k) { d[k] = opdd(d[2*k], d[2*k+1]); }
+
+    D sum(int a, int b, int l, int r, int k) {
+        if (b <= l || r <= a) return e_d;
+        if (a <= l && r <= b) return d[k];
+        push(k);
+        int mid = (l + r) / 2;
+        return op_dd(sum(a, b, l, mid, 2*k), sum(a, b, mid, r, 2*k+1));
+    }
+    D sum(int a, int b) { return sum(a, b, 0, sz, 1); }
+
+    void add(int a, int b, L x, int l, int r, int k) {
+        if (b <= l || r <= a) return;
+        if (a <= l && r <= b) {
+            all_add(k, x);
+            return;
+        }
+        push(k);
+        int mid = (l + r) / 2;
+        add(a, b, x, l, mid, 2*k); add(a, b, x, mid, r, 2*k+1);
+        update(k);
+    }
+    void add(int a, int b, L x) { add(a, b, x, 0, sz, 1); }
+
+};
