52a26c1887f54ec211aaff1f8ec3eef2acb7a854
Kohei Morita <yosupo06@gmail.com>
2018-12-15 20:30:11 +0900

    fix fasthash

--- src/util/hash.h
+++ src/util/hash.h
@@ -1,22 +1,31 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 
-static inline uint64_t mix(uint64_t h) {
+uint64_t mix(uint64_t h) {
     h ^= h >> 23;
     h *= 0x2127599bf4325c37ULL;
     h ^= h >> 47;
     return h;
 }
 
-ull hash_f(ull x) {
-    const uint64_t    m = 0x880355f21e6d1965ULL;
+uint64_t hash_f(ull x) {
+    const uint64_t m = 0x880355f21e6d1965ULL;
     return mix((m ^ mix(x)) * m);
 }
 
-struct fasthash {
-    ull operator()(ll x) const { return hash_f(x); }
+template <class T> struct fasthash {};
+
+template <> struct fasthash<int> {
+    size_t operator()(int x) const { return hash_f(x); }
+};
+template <> struct fasthash<uint> {
+    size_t operator()(uint x) const { return hash_f(x); }
+};
+template <> struct fasthash<ll> {
+    size_t operator()(ll x) const { return hash_f(x); }
+};
+template <> struct fasthash<ull> {
+    size_t operator()(ull x) const { return hash_f(x); }
 };
 
-
-template <class K, class D>
-using hashmap = gp_hash_table<K, D, fasthash>;
+template <class K, class D> using hashmap = gp_hash_table<K, D, fasthash<K>>;
