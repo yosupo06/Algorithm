7faff38b05a9aed12637617005b5743caca94697
Kohei Morita <yosupo06@gmail.com>
2018-12-27 17:03:00 +0900

    small fix

--- src/math/modint.h
+++ src/math/modint.h
@@ -1,14 +1,14 @@
 template <uint MD> struct ModInt {
     using M = ModInt;
     uint v;
-    ModInt() : v{0} {}
+    ModInt() : v(0) {}
     ModInt(ll _v) { set_v(_v % MD + MD); }
     M& set_v(uint _v) {
         v = (_v < MD) ? _v : _v - MD;
         return *this;
     }
     explicit operator bool() const { return v != 0; }
-    M operator-() const { return M(0) - *this; }
+    M operator-() const { return M() - *this; }
     M operator+(const M& r) const { return M().set_v(v + r.v); }
     M operator-(const M& r) const { return M().set_v(v + MD - r.v); }
     M operator*(const M& r) const { return M().set_v(ull(v) * r.v % MD); }
@@ -26,6 +26,6 @@ template <uint MD> struct ModInt {
         }
         return r;
     }
-    M inv() const { return (*this).pow(MD - 2); }
+    M inv() const { return pow(MD - 2); }
     friend ostream& operator<<(ostream& os, const M& r) { return os << r.v; }
 };
--- test/math/modint_test.cpp
+++ test/math/modint_test.cpp
@@ -7,9 +7,24 @@ TEST(ModIntYosupo, Usage) {
     using Mint = ModInt<MD>;
 
     Mint a = 5, b = 10;
-    ASSERT_EQ((a+b).v, 2); // (100 + 10) % 107 == 3
-    ASSERT_EQ((a-b).v, 8); // (100 - 10) % 107 == 90
-    ASSERT_EQ((a*b).v, 11); // (100 - 10) % 107 == 90
-    ASSERT_EQ((a/b).v, 7); // (100 - 10) % 107 == 90
+    ASSERT_EQ((a + b).v, 2); // (100 + 10) % 107 == 3
+    ASSERT_EQ((a - b).v, 8); // (100 - 10) % 107 == 90
+    ASSERT_EQ((a * b).v, 11); // (100 - 10) % 107 == 90
+    ASSERT_EQ((a / b).v, 7); // (100 - 10) % 107 == 90
+}
+
+TEST(ModIntYosupo, SomeFunc) {
+    const int MD = 13;
+    using Mint = ModInt<MD>;
+    Mint m;
+    ASSERT_EQ(m.v, 0);
+    m += Mint(3);
+    ASSERT_EQ(m.v, 3);
+    m -= Mint(4);
+    ASSERT_EQ(m.v, 12);
+    m *= Mint(2);
+    ASSERT_EQ(m.v, 11);
     ASSERT_EQ(Mint(2).pow(4).v, 3);
+    ASSERT_EQ((Mint(4).inv() * Mint(4)).v, 1);
+    ASSERT_EQ(Mint(20).v, 7);
 }
