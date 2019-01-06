0af97bcb69a03dd97c3b598617d48a15b5bb1f9a
Kohei Morita <yosupo06@gmail.com>
2019-01-07 00:53:53 +0900

    fix alltree

--- src/tree/alltree.h
+++ src/tree/alltree.h
@@ -9,9 +9,9 @@ template <class N, class E> struct AllTree {
             int d = e.to;
             if (d == b) continue;
             dfs1(d, p);
-            sm[p] = sm[p] + sm[d].first(p, e);
+            sm[p] = sm[p] + sm[d].to_subs(p, e);
         }
-        sm[p].finish(p);
+        sm[p].join(p);
     }
     void dfs2(int p, int b, N top) {
         int deg = int(g[p].size());
@@ -19,16 +19,16 @@ template <class N, class E> struct AllTree {
         dp[p][0] = N();
         for (int i = 0; i < deg; i++) {
             int d = g[p][i].to;
-            dp[p][i + 1] = dp[p][i] + (d == b ? top : sm[d]).first(p, g[p][i]);
+            dp[p][i + 1] = dp[p][i] + (d == b ? top : sm[d]).to_subs(p, g[p][i]);
         }
         N rnode = N();
         for (int i = deg - 1; i >= 0; i--) {
             int d = g[p][i].to;
             if (d != b) {
-                dfs2(d, p, (dp[p][i] + rnode).finish(p));
+                dfs2(d, p, (dp[p][i] + rnode).join(p));
             }
             dp[p][i] = dp[p][i] + rnode;
-            rnode = rnode + (d == b ? top : sm[d]).first(p, g[p][i]);
+            rnode = rnode + (d == b ? top : sm[d]).to_subs(p, g[p][i]);
         }
     }
     AllTree(const VV<E>& _g) : n(int(_g.size())), g(_g), sm(n), dp(n) {
@@ -36,46 +36,33 @@ template <class N, class E> struct AllTree {
         dfs2(0, -1, N());
     }
 };
+
 template <class N, class E> VV<N> get_all_tree(const VV<E>& g) {
     return AllTree<N, E>(g).dp;
 }
 
+//TODO : add sample, calc radius of tree
 struct Node {
-    // サンプル 木の直径
-    // firstは 根付き木->森
-    // finishは森->根付き木
-    int rad, dia;
-    int rd[2];
+    //Educational DP Contest V - Subtree
+    Mint sm = Mint(0);
     Node() {
-        // !!!!!森を生成すること!!!!!
-        rad = 0;
-        dia = 0;
-        rd[0] = rd[1] = 0;
+        //empty subtree
+        sm = 1;
     }
-    template <class E> Node first(int par, const E& e) {
-        // 根付き木->森 上に辺を追加するイメージ e=(par -> node)
-        rd[0] = rad + e.dist;
-        rd[1] = 0;
-        return *this;
-    }
-    Node operator+(const Node& r) {
-        // 結合則が必須 上に辺を追加された木をまとめるイメージ
-        Node n;
-        vector<int> v;
-        v.push_back(rd[0]);
-        v.push_back(rd[1]);
-        v.push_back(r.rd[0]);
-        v.push_back(r.rd[1]);
-        sort(begin(v), end(v), greater<int>());
-        n.rd[0] = v[0];
-        n.rd[1] = v[1];
-        n.dia = max(dia, r.dia);
+    template <class E> Node to_subs(int, const E&) const {
+        //tree -> subtrees
+        Node n = *this;
+        n.sm += Mint(1);
         return n;
     }
-    Node finish(int par) {
-        // 元締めとしてidxを追加するイメージ
-        rad = rd[0];
-        dia = max(dia, rd[0] + rd[1]);
+    Node operator+(const Node& r) const {
+        //subtrees + subtrees
+        Node n;
+        n.sm = sm * r.sm;
+        return n;
+    }
+    Node join(int) const {
+        //subtrees -> tree
         return *this;
     }
 };
