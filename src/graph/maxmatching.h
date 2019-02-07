094184442c7331e951330473da6c4aca58936565
Kohei Morita <yosupo06@gmail.com>
2019-02-07 11:13:59 +0900

    fix maxmatching

--- src/graph/maxmatching.h
+++ src/graph/maxmatching.h
@@ -1,18 +1,6 @@
-// !! Broken !!
-
-#include <bits/stdc++.h>
-
-using namespace std;
-using uint = unsigned int;
-using ll = long long;
-using ull = unsigned long long;
-constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n - 1); }
-template <class T> using V = vector<T>;
-template <class T> using VV = V<V<T>>;
-
-//TODO: gabow
-template<class E>
-struct MaxMatching {
+// Gabow Edmond's blossom algorithm
+// Reference: https://qiita.com/Kutimoti_T/items/5b579773e0a24d650bdf
+template <class E> struct MaxMatching {
     int n;
     const VV<E>& g;
     V<int> mt;
@@ -41,25 +29,31 @@ struct MaxMatching {
 
     bool arg(int st) {
         ty[st] = gr_buf[st] = -1;
+        nx[st] = P(-1, -1);
         queue<int> q;
         V<int> vis;
-        q.push(st); vis.push_back(st);
+        q.push(st);
+        vis.push_back(st);
         while (q.size()) {
-            int a = q.front(); q.pop();
-            for (auto e: g[a]) {
+            int a = q.front();
+            q.pop();
+            for (auto e : g[a]) {
                 int b = e.to;
-                if (ty[b] == 1) continue; // odd point
+                if (ty[b] == 1) continue;  // odd point
                 if (ty[b] == 0) {
                     if (mt[b] == -1) {
-                        //increase matching
-                        mt[b] = a; match(a, b);
-                        for (int d: vis) ty[d] = 0;
+                        // increase matching
+                        mt[b] = a;
+                        match(a, b);
+                        for (int d : vis) ty[d] = 0;
                         return true;
                     }
                     // connect
-                    vis.push_back(b); vis.push_back(mt[b]);
+                    vis.push_back(b);
+                    vis.push_back(mt[b]);
                     ty[b] = 1;
                     ty[mt[b]] = -1;
+                    nx[b] = P(-1, -1);
                     nx[mt[b]] = P(a, -1);
                     gr_buf[mt[b]] = b;
                     q.push(mt[b]);
@@ -70,160 +64,31 @@ struct MaxMatching {
                 if (x != -1) nx[x] = P(a, b);
                 if (y != -1) nx[y] = P(a, b);
                 int z = -1;
-                while (true) {
+                while (x != -1 || y != -1) {
                     if (y != -1) swap(x, y);
                     x = group(nx[mt[x]].first);
-                    if (x == -1 || nx[x] == P(a, b)) {
+                    if (x == -1) continue;
+                    if (nx[x] == P(a, b)) {
                         z = x;
                         break;
                     }
                     nx[x] = P(a, b);
                 }
-                for (int v : {gr_buf[a], gr_buf[b]}) {
-                    while (v != -1 && v != z) {
+                for (int v : {group(a), group(b)}) {
+                    while (v != z) {
                         q.push(v);
                         ty[v] = -1;
-                        nx[v] = P(a, b);
                         gr_buf[v] = z;
-                        v = gr_buf[nx[mt[v]].first];
+                        v = group(nx[mt[v]].first);
                     }
                 }
             }
         }
         return false;
     }
-    MaxMatching(const VV<E>& _g) : n(int(_g.size())), g(_g), mt(n, -1), ty(n, 0), gr_buf(n), nx(n) {
-        int ans = 0;
-        for (int st = 0; st < n; st++) {
-            if (mt[st] == -1 && arg(st)) {
-//                fill(ty.begin(), ty.end(), 0);
-                ans++;
-            }
-        }
+    MaxMatching(const VV<E>& _g)
+        : n(int(_g.size())), g(_g), mt(n, -1), ty(n, 0), gr_buf(n), nx(n) {
+        for (int st = 0; st < n; st++)
+            if (mt[st] == -1) arg(st);
     }
 };
-using P = pair<int, int>; //r, c
-struct E { int to; };
-
-void solve(V<string> mp) {
-    int h = int(mp.size()), w = int(mp[0].size());
-    int n = 0;
-    VV<E> g;
-    auto inc_n = [&]() {
-        g.push_back({});
-        return n++;
-    };
-    auto add_e = [&](int a, int b) {
-        if (a == -1 || b == -1) return;
-        assert(a < n && b < n);
-        g[a].push_back({b});
-        g[b].push_back({a});
-    };
-    map<P, int> to_id;
-    map<int, P> to_p;
-    auto get_id = [&](int r, int c) {
-        if (to_id.count(P(r, c))) return to_id[P(r, c)];
-        return -1;
-    };
-    for (int r = 0; r < h; r++) {
-        for (int c = 0; c < w; c++) {
-            if (mp[r][c] != '.') continue;
-            int id = inc_n();
-            to_id[P(r, c)] = id;
-            to_p[id] = P(r, c);
-        }
-    }
-    VV<int> id1s(h, V<int>(w)), id2s(h, V<int>(w));
-
-    for (int r = 0; r < h; r++) {
-        for (int c = 0; c < w; c++) {
-            if (mp[r][c] == '.') continue;
-            int id1 = inc_n(), id2 = inc_n();
-            add_e(id1, id2);
-            id1s[r][c] = id1;
-            id2s[r][c] = id2;
-            if (mp[r][c] == '+') {
-                add_e(id1, get_id(r, c - 1));
-                add_e(id1, get_id(r - 1, c));
-                add_e(id1, get_id(r, c + 1));
-                add_e(id1, get_id(r + 1, c));
-
-                add_e(id2, get_id(r, c - 1));
-                add_e(id2, get_id(r - 1, c));
-                add_e(id2, get_id(r, c + 1));
-                add_e(id2, get_id(r + 1, c));
-            } else {
-                add_e(id1, get_id(r, c - 1));
-                add_e(id1, get_id(r, c + 1));
-
-                add_e(id2, get_id(r - 1, c));
-                add_e(id2, get_id(r + 1, c));
-            }
-        }
-    }
-
-    MaxMatching<E> mts(g);
-
-    auto mt = mts.mt;
-
-    V<string> ans = mp;
-    auto bc = [&](int r, int c) {
-        return 0 <= r && r < h && 0 <= c && c < w;
-    };
-    auto mark = [&](int r, int c, P p1, P p2) {
-        set<char> s;
-        for (char u = 'a'; u <= 'z'; u++) s.insert(u);
-        for (int dr = -2; dr <= 2; dr++) {
-            for (int dc = -2; dc <= 2; dc++) {
-                if (bc(r + dr, c + dc)) s.erase(ans[r + dr][c + dc]);
-            }
-        }
-        auto u = *s.begin();
-        ans[r][c] = u;
-        ans[p1.first][p1.second] = u;
-        ans[p2.first][p2.second] = u;
-    };
-    for (int r = 0; r < h; r++) {
-        for (int c = 0; c < w; c++) {
-            if (mp[r][c] == '.') continue;
-            int id1 = id1s[r][c], id2 = id2s[r][c];
-            if (mt[id1] == id2 || mt[id1] == -1 || mt[id2] == -1) continue;
-            mark(r, c, to_p[mt[id1]], to_p[mt[id2]]);
-        }
-    }
-    cout << "END" << endl;
-    //for (auto s: ans) cout << s << endl;
-}
-
-int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false); //warning!
-    cout << setprecision(20) << fixed;
-
-    for (int ph = 0; ph < TEN(6); ph++) {
-        int h, w;
-        h = rand() % 10 + 1;
-        w = rand() % 10 + 1;
-        //cin >> h >> w;
-        V<string> mp(h);
-        //for (int r = 0; r < h; r++) cin >> mp[r];
-        for (int r = 0; r < h; r++) {
-            for (int c = 0; c < w; c++) {
-                int ty = rand() % 3;
-                if (ty == 0) mp[r] += '+';
-                else if (ty == 1) mp[r] += '.';
-                else mp[r] += '*';
-            }
-        }
-        cout << h << " " << w << endl;
-        for (string s: mp) cout << s << endl;
-        solve(mp);
-    }
-
-    int h, w;
-    cin >> h >> w;
-    V<string> mp(h);
-    for (int r = 0; r < h; r++) cin >> mp[r];
-    solve(mp);
-    return 0;
-}
---
+++ test/graph/maxmatch_test.cpp
@@ -0,0 +1,167 @@
+#include "gtest/gtest.h"
+#include "base.h"
+#include "random.h"
+#include "util/ostream.h"
+#include "graph/maxmatching.h"
+
+template<class E>
+struct MaxMatchingNaive {
+    int n;
+    const VV<E>& g;
+    V<int> mt;
+    MaxMatchingNaive(const VV<E>& _g) : n(int(_g.size())), g(_g), mt(n, -1) {
+        while (arg()) {};
+    }
+    bool arg() {
+        V<int> ty(n), rt(n, -1), gr(n, -1), nw(n, -1);
+
+        queue<int> q; // 偶点用のqueue
+        for (int i = 0; i < n; i++) {
+            if (mt[i] == -1) {
+                rt[i] = gr[i] = i; // 木での根, 基点
+                ty[i] = -1;
+                q.push(i);
+            }
+        }
+
+        // uからのパスを返す
+        auto get_path = [&](int u) {
+            V<int> v;
+            while (rt[u] != u) {
+                v.push_back(u);
+                v.push_back(mt[u]);
+                u = nw[mt[u]];
+            }
+            v.push_back(u);
+            return v;
+        };
+
+        while (q.size()) {
+            int a = q.front(); q.pop();
+            for (auto e: g[a]) {
+                int b = e.to;
+                if (ty[b] == 1) continue; // 辺が無い or 奇点ならスルー
+                if (ty[b] == -1 and gr[a] == gr[b]) continue; // 偶点でもgropが同じならスルー
+                if (ty[b] == 0) {
+                    // つなぐ
+                    int c = mt[b];
+                    nw[b] = a; nw[c] = -1;
+                    rt[b] = rt[c] = rt[a];
+                    gr[b] = b; gr[c] = c;
+                    ty[b] = 1; ty[c] = -1;
+                    q.push(c);
+                    continue;
+                }
+                if (rt[a] != rt[b]) {
+                    // パスを見つける
+                    for (int c: {a, b}) {
+                        auto v = get_path(c);
+                        int m = int(v.size());
+                        assert(m % 2 == 1);
+                        for (int i = 1; i < m; i += 2) {
+                            mt[v[i]] = v[i+1];
+                            mt[v[i+1]] = v[i];
+                        }
+                    }
+                    mt[a] = b; mt[b] = a;
+                    return true;
+                }
+                // 潰す
+                auto v1 = get_path(a);
+                auto v2 = get_path(b);
+                reverse(v1.begin(), v1.end());
+                reverse(v2.begin(), v2.end());
+                assert(v1[0] == v2[0]);
+                int m = min(v1.size(), v2.size()), lca;
+                for (int i = 1; i <= m; i++) {
+                    if (i == m || v1[i] != v2[i]) {
+                        lca = gr[v1[i-1]]; // LCA
+                        break;
+                    }
+                }
+                V<bool> used(n);
+                for (int c: {a, b}) {
+                    auto v = get_path(c);
+                    for (int i = 0; gr[v[i]] != lca; i += 2) {
+                        used[gr[v[i]]] = used[gr[v[i+1]]] = true;
+                        if (ty[v[i+1]] == 1) {
+                            ty[v[i+1]] = -1;
+                            q.push(v[i+1]);
+                        }
+                        if (i) nw[v[i]] = v[i-1];
+                    }
+                }
+                if (gr[a] != lca) nw[a] = b;
+                if (gr[b] != lca) nw[b] = a;
+                for (int i = 0; i < n; i++) {
+                    if (ty[i] && used[gr[i]]) {
+                        gr[i] = lca;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+int check_mt(const VV<int>& mp, const V<int>& mt) {
+    int n = int(mt.size());
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (mt[i] == -1) continue;
+        if (mt[i] == i || mt[mt[i]] != i || !mp[i][mt[i]]) {
+            cerr << mt << endl;
+            cerr << (mt[i] == i) << " " << (mt[mt[i]] != i) << " " << mp[i][mt[i]] << endl;
+            return -1;
+        }
+        ans++;
+    }
+    return ans / 2;
+}
+
+TEST(MatchingTest, ManyRandom) {
+    Random gen;
+    struct E {
+        int to;
+    };
+
+    for (int ph = 0; ph < 100000; ph++) {
+        int n = gen.uniform(2, 20);
+        int m = gen.uniform(0, 30);
+        VV<int> mp(n, V<int>(n));
+        VV<E> g(n);
+
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            while (true) {
+                x = gen.uniform(0, n-1);
+                y = gen.uniform(0, n-1);
+                if (x == y) continue;
+                break;
+            }
+            if (mp[x][y]) continue;
+            mp[x][y] = mp[y][x] = true;
+            g[x].push_back({y});
+            g[y].push_back({x});
+        }
+
+        auto res1 = MaxMatching<E>(g).mt;
+        auto res2 = MaxMatchingNaive<E>(g).mt;
+
+        int v1 = check_mt(mp, res1);
+        int v2 = check_mt(mp, res2);
+
+        if (v1 == -1) {
+            for (int i = 0; i < n; i++) {
+                for (E e: g[i]) {
+                    int j = e.to;
+                    if (i > j) continue;
+                    cout << i << ", " << j << endl;
+                }
+            }
+        }
+        EXPECT_NE(v1, -1);
+        EXPECT_NE(v2, -1);
+        EXPECT_EQ(v1, v2);
+    }
+}
