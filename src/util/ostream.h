a130a827f6db68ee5171751800ae88d2593e2de0
Kohei Morita <yosupo06@gmail.com>
2019-03-06 09:11:39 +0900

    add pair ostream

--- src/util/ostream.h
+++ src/util/ostream.h
@@ -1,10 +1,10 @@
+template <class T, class U>
+ostream& operator<<(ostream& os, const pair<T, U>& p) {
+    return os << "P(" << p.first << ", " << p.second << ")";
+}
+
 template <class T> ostream& operator<<(ostream& os, const V<T>& v) {
     os << "[";
     for (auto d : v) os << d << ", ";
     return os << "]";
-}
-
-template <class T, class U>
-ostream& operator<<(ostream& os, const pair<T, U>& p) {
-    return os << "P(" << p.first << ", " << p.second << ")";
 }
