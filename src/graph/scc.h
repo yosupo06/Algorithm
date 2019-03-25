48b16135fb2bfc090dd30afa9ae8e2d9332dd7ca
Kohei Morita <yosupo06@gmail.com>
2019-03-25 10:22:23 +0900

    rm unused scc

--- src/graph/scc.h
+++ src/graph/scc.h
@@ -48,47 +48,7 @@ template <class E> struct SCCExec : SCC {
     }
 };
 
-template <class E> struct KosarajuSCCExec : SCC {
-    int n;
-    const VV<E>& g;
-    const VV<E>& rg;
-    V<int> vs;
-    V<bool> used;
-    void dfs(int v) {
-        used[v] = true;
-        for (E e : g[v]) {
-            if (!used[e.to]) dfs(e.to);
-        }
-        vs.push_back(v);
-    }
-
-    void rdfs(int v, int k) {
-        used[v] = true;
-        id[v] = k;
-        groups[k].push_back(v);
-        for (E e : rg[v]) {
-            if (!used[e.to]) rdfs(e.to, k);
-        }
-    }
-
-    KosarajuSCCExec(const VV<E>& _g, const VV<E>& _rg)
-        : n(int(_g.size())), g(_g), rg(_rg) {
-        used = V<bool>(n);
-        for (int i = 0; i < n; i++) {
-            if (!used[i]) dfs(i);
-        }
-        reverse(vs.begin(), vs.end());
-        used = V<bool>(n);
-        id = V<int>(n);
-        int k = 0;
-        for (int i : vs) {
-            if (!used[i]) {
-                groups.push_back({});
-                rdfs(i, k++);
-            }
-        }
-    }
-};
+template <class E> SCC get_scc(const VV<E>& g) { return SCCExec<E>(g); }
 
 template <size_t N> struct BitsetSCCExec : SCC {
     using B = bitset<N>;
@@ -137,10 +97,6 @@ template <size_t N> struct BitsetSCCExec : SCC {
     }
 };
 
-template <class E> SCC get_scc(const VV<E>& g) { return SCCExec<E>(g); }
-template <class E> SCC get_kosaraju_scc(const VV<E>& g, const VV<E>& rg) {
-    return KosarajuSCCExec<E>(g, rg);
-}
 template <size_t N>
 SCC get_bitset_scc(const V<bitset<N>>& g, const V<bitset<N>>& rg) {
     return BitsetSCCExec<N>(g, rg);
