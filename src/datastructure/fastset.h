1d87373d57ac18a182ff7dbed5482307f2c58899
Kohei Morita <yosupo06@gmail.com>
2019-03-25 18:18:24 +0900

    fix fastset

--- src/datastructure/fastset.h
+++ src/datastructure/fastset.h
@@ -1,34 +1,34 @@
 struct FastSet {
-    static constexpr size_t B = 64;
-    size_t n, lg;
+    static constexpr uint B = 64;
+    int n, lg;
     VV<ull> seg;
-    FastSet(size_t _n) : n(_n) {
+    FastSet(int _n) : n(_n) {
         do {
             seg.push_back(V<ull>((_n + B - 1) / B));
             _n = (_n + B - 1) / B;
         } while (_n > 1);
         lg = seg.size();
     }
-    bool operator[](size_t i) const {
+    bool operator[](int i) const {
         return (seg[0][i / B] >> (i % B) & 1) != 0;
     }
-    void set(size_t i) {
-        for (size_t h = 0; h < lg; h++) {
+    void set(int i) {
+        for (int h = 0; h < lg; h++) {
             seg[h][i / B] |= 1ULL << (i % B);
             i /= B;
         }
     }
-    void reset(size_t i) {
-        for (size_t h = 0; h < lg; h++) {
+    void reset(int i) {
+        for (int h = 0; h < lg; h++) {
             seg[h][i / B] &= ~(1ULL << (i % B));
             if (seg[h][i / B]) break;
             i /= B;
         }
     }
     // x以上最小の要素
-    ssize_t next(size_t i) {
-        for (ssize_t h = 0; h < ssize_t(lg); h++) {
-            if (i / 64 == seg[h].size()) break;
+    int next(int i) {
+        for (int h = 0; h < lg; h++) {
+            if (i / B == seg[h].size()) break;
             ull d = seg[h][i / B] >> (i % B);
             if (!d) {
                 i = i / B + 1;
@@ -36,8 +36,8 @@ struct FastSet {
             }
             // find
             i += bsf(d);
-            for (ssize_t g = h - 1; g >= 0; g--) {
-                i *= 64;
+            for (int g = h - 1; g >= 0; g--) {
+                i *= B;
                 i += bsf(seg[g][i / B]);
             }
             return i;
@@ -45,20 +45,20 @@ struct FastSet {
         return n;
     }
     // x未満最大の要素
-    ssize_t prev(ssize_t i) {
+    int prev(int i) {
         i--;
-        for (ssize_t h = 0; h < ssize_t(lg); h++) {
+        for (int h = 0; h < lg; h++) {
             if (i == -1) break;
             ull d = seg[h][i / B] << (63 - i % 64);
             if (!d) {
-                i = i / 64 - 1;
+                i = i / B - 1;
                 continue;
             }
             // find
-            i += bsr(d) - 63;
-            for (ssize_t g = h - 1; g >= 0; g--) {
-                i *= 64;
-                i += bsr(seg[g][i / 64]);
+            i += bsr(d) - (B - 1);
+            for (int g = h - 1; g >= 0; g--) {
+                i *= B;
+                i += bsr(seg[g][i / B]);
             }
             return i;
         }
