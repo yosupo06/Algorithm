1e109676c906252e83552e0d630410fe6c7fa077
Kohei Morita <yosupo06@gmail.com>
2019-03-25 17:43:43 +0900

    fix frac

--- src/math/frac.h
+++ src/math/frac.h
@@ -25,6 +25,47 @@ template <class I> struct Frac {
     bool operator>(const Frac& r) const { return r < *this; }
     bool operator<=(const Frac& r) const { return !(r < *this); }
     bool operator>=(const Frac& r) const { return !(*this < r); }
-    bool operator==(const Frac& r) const { return a * r.b == b * r.a; }
+    bool operator==(const Frac& r) const {
+        return !(*this < r) && !(r < *this);
+    }
     bool operator!=(const Frac& r) const { return !(*this == r); }
+
+    static Frac rec(Frac x, Frac y, Frac l, Frac r) {
+        auto flip = [&](Frac& f) { f = Frac(1) - f; };
+        auto cross = [&](const Frac& f, const Frac& g) {
+            return f.a * g.b - f.b * g.a;
+        } Frac m = {l.a + r.a, l.b + r.b};
+        if (x < m && m < y) return m;
+        bool s = !(x < m);
+        if (s) {
+            flip(l);
+            flip(r);
+            flip(m);
+            flip(x);
+            flip(y);
+            swap(l, r);
+            swap(x, y);
+        }
+        int k = cross(r, y) / cross(y, l) + 1;
+        Frac p = {k * l.a + r.a, k * l.b + r.b};
+        if (x < p) {
+            if (s) flip(p);
+            return p;
+        }
+        Frac q = rec(x, y, p, {(k - 1) * l.a + r.a, (k - 1) * l.b + r.b});
+        if (s) flip(q);
+        return q;
+    }
+    static Frac in_bet(Frac x, Frac y) {
+        Frac ret;
+        I num = x.a >= 0 ? x.a / x.b : -((x.b - 1 - x.a) / x.b);
+        x.a -= x.b * num;
+        y.a -= y.b * num;
+        if (Frac{1, 1} < y)
+            ret = Frac{1, 1};
+        else
+            ret = rec(x, y, Frac{0, 1}, Frac{1, 1});
+        ret.a += ret.b * num;
+        return ret;
+    }
 };
