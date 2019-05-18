8d745fb863818e1f31f9d5b8eb5542f305918def
Kohei Morita <yosupo06@gmail.com>
2019-05-18 22:43:11 +0900

    fix template

--- src/base.h
+++ src/base.h
@@ -9,6 +9,23 @@ constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n - 1); }
 template <class T> using V = vector<T>;
 template <class T> using VV = V<V<T>>;
 
+#ifdef LOCAL
+#define show(x) cerr << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define show(x) true
+#endif
+
+template <class T, class U>
+ostream& operator<<(ostream& os, const pair<T, U>& p) {
+    return os << "P(" << p.first << ", " << p.second << ")";
+}
+
+template <class T> ostream& operator<<(ostream& os, const V<T>& v) {
+    os << "[";
+    for (auto d : v) os << d << ", ";
+    return os << "]";
+}
+
 /*
 int main() {
     cin.tie(nullptr);
