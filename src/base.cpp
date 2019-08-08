2b0e509e6c41d3fd1e5c4c8fce8e21d2dd799b93
Kohei Morita <yosupo06@gmail.com>
2019-08-09 06:57:24 +0900

    add base.cpp

---
+++ src/base.cpp
@@ -0,0 +1,38 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx")
+//#undef LOCAL
+#include <bits/stdc++.h>
+
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n - 1); }
+template <class T> using V = vector<T>;
+template <class T> using VV = V<V<T>>;
+
+#ifdef LOCAL
+#define show(x) cerr << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define show(x)
+#endif
+
+template <class T, class U>
+ostream& operator<<(ostream& os, const pair<T, U>& p) {
+    return os << "P(" << p.first << ", " << p.second << ")";
+}
+
+template <class T> ostream& operator<<(ostream& os, const V<T>& v) {
+    os << "[";
+    for (auto d : v) os << d << ", ";
+    return os << "]";
+}
+
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    return 0;
+}
+
