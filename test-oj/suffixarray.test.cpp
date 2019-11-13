2ec64a33aecca02dd9716ea83ed9b39df92d0939
Kohei Morita <yosupo06@gmail.com>
2019-11-14 01:06:19 +0900

    add oj test

---
+++ test-oj/suffixarray.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/suffixarray"
+
+#include "../src/base.h"
+#include "../src/string/suffixarray.h"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    auto sa = sa_is(s);
+    int n = int(s.size());
+    for (int i = 1; i <= n; i++) cout << sa[i] << " ";
+    cout << endl;
+}
---
+++ test-oj/test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+oj --version
+
+CXX=${CXX:-g++}
+CXXFLAGS="${CXXFLAGS:--std=c++14 -O2 -Wall -g}"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    CXXFLAGS="$CXXFLAGS -Wl,-stack_size,0x10000000"
+else
+    ulimit -s unlimited
+fi
+
+run() {
+    file="$1"
+    url=$(grep -o '^# *define \+PROBLEM \+\"\(https\?://.*\)\"' < "$file" | sed 's/.* "http/"http/')
+    url=${url%\"}
+    url=${url#\"}
+    dir=test/$(echo -n $url | md5sum | sed 's/ .*//')
+    mkdir -p ${dir}
+    $CXX $CXXFLAGS -I . -o ${dir}/a.out "$file"
+    if [[ -n ${url} ]] ; then
+        if [[ ! -e ${dir}/test ]] ; then
+            sleep 2
+            oj download --system $url -d ${dir}/test
+        fi
+        oj test --tle 10 --c ${dir}/a.out -d ${dir}/test
+    else
+        ${dir}/a.out
+    fi
+}
+
+if [ $# -eq 0 ] ; then
+    for f in $(find . -name \*.test.cpp) ; do
+        run $f
+    done
+else
+    for f in "$@" ; do
+        run "$f"
+    done
+fi
