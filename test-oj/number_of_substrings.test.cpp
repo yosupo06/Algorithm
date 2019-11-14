12f709699faba1ac532e3e746bf72ce0b4bcd797
Kohei Morita <yosupo06@gmail.com>
2019-11-14 13:26:32 +0900

    add number_of_substrings

---
+++ test-oj/number_of_substrings.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/number_of_substrings"
+
+#include "src/base.h"
+#include "src/string/suffixarray.h"
+
+int main() {
+    Scanner sc(stdin);
+    Printer pr(stdout);
+    string s;
+    sc.read(s);
+    auto sai = SA<string>(s, sa_is(s));
+    int n = int(s.size());
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += max(0, (n - sai.sa[i + 1]) - sai.lcp[i]);
+    }
+    pr.writeln(ans);
+}
