5e029798ba6d145950a7f96ce1b5e2f78665f98e
Kohei Morita <yosupo06@gmail.com>
2019-11-30 22:48:02 +0900

    fix base

--- expander/expander.py
+++ expander/expander.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
         level=getenv('LOG_LEVEL', 'INFO'),
         format="%(asctime)s %(levelname)s %(name)s : %(message)s"
     )
-    parser = argparse.ArgumentParser(description='Testcase Generator')
+    parser = argparse.ArgumentParser(description='Source Code Combiner')
     parser.add_argument('src', help='Source File')
+    parser.add_argument('out', help='Output File')
     args = parser.parse_args()
 
 
@@ -23,9 +24,9 @@ if __name__ == "__main__":
         '-I/Users/yosupo/Programs/Algorithm/src',
         '-nostdlib',
         '-I/Users/yosupo/Programs/Algorithm/expander/dummy_include',
-        '-o', 'connected.cpp'])
+        '-o', args.out])
 
-    f = open('connected.cpp', 'r')
+    f = open(args.out, 'r')
     pat = re.compile(r'#pragma yosupo_dummy "(.*)"')
     new_src = ""
     for s in f.readlines():
@@ -35,6 +36,6 @@ if __name__ == "__main__":
         else:
             new_src += s
     f.close()
-    f = open('connected.cpp', 'w')
+    f = open(args.out, 'w')
     f.write(new_src)
     f.close()
--- src/base.cpp
+++ src/base.cpp
@@ -4,14 +4,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#pragma region template
-
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
 constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n - 1); }
 template <class T> using V = vector<T>;
 template <class T> using VV = V<V<T>>;
+
+#include "util/fast_io.h"
 
 #ifdef LOCAL
 struct PrettyOS {
@@ -48,139 +48,10 @@ template <class T> ostream& operator<<(ostream& os, const V<T>& v) {
     return os << "]";
 }
 
-struct Scanner {
-    FILE* fp = nullptr;
-    char line[(1 << 15) + 1];
-    size_t st = 0, ed = 0;
-    void reread() {
-        memmove(line, line + st, ed - st);
-        ed -= st;
-        st = 0;
-        ed += fread(line + ed, 1, (1 << 15) - ed, fp);
-        line[ed] = '\0';
-    }
-    bool succ() {
-        while (true) {
-            if (st == ed) {
-                reread();
-                if (st == ed) return false;
-            }
-            while (st != ed && isspace(line[st])) st++;
-            if (st != ed) break;
-        }
-        if (ed - st <= 50) reread();
-        return true;
-    }
-    template <class T, enable_if_t<is_same<T, string>::value, int> = 0>
-    bool read_single(T& ref) {
-        if (!succ()) return false;
-        while (true) {
-            succ();
-            size_t sz = 1;
-            while (st + sz < ed && !isspace(line[st + sz])) sz++;
-            ref.append(line + st, sz);
-            st += sz;
-            if (st != ed) break;
-        }
-        return true;
-    }
-    template <class T, enable_if_t<is_integral<T>::value, int> = 0>
-    bool read_single(T& ref) {
-        if (!succ()) return false;
-        bool neg = false;
-        if (line[st] == '-') {
-            neg = true;
-            st++;
-        }
-        ref = T(0);
-        while (isdigit(line[st])) {
-            ref = 10 * ref + (line[st++] - '0');
-        }
-        if (neg) ref = -ref;
-        return true;
-    }
-    template <class T> bool read_single(V<T>& ref) {
-        for (auto& d : ref) {
-            if (!read_single(d)) return false;
-        }
-        return true;
-    }
-    void read() {}
-    template <class H, class... T> void read(H& h, T&... t) {
-        bool f = read_single(h);
-        assert(f);
-        read(t...);
-    }
-    Scanner(FILE* _fp) : fp(_fp) {}
-};
 
-struct Printer {
-  public:
-    template <bool F = false> void write() {}
-    template <bool F = false, class H, class... T>
-    void write(const H& h, const T&... t) {
-        if (F) write_single(' ');
-        write_single(h);
-        write<true>(t...);
-    }
-    template <class... T> void writeln(const T&... t) {
-        write(t...);
-        write_single('\n');
-    }
-
-    Printer(FILE* _fp) : fp(_fp) {}
-    ~Printer() { flush(); }
-
-  private:
-    static constexpr size_t SIZE = 1 << 15;
-    FILE* fp;
-    char line[SIZE], small[50];
-    size_t pos = 0;
-    void flush() {
-        fwrite(line, 1, pos, fp);
-        pos = 0;
-    }
-    void write_single(const char& val) {
-        if (pos == SIZE) flush();
-        line[pos++] = val;
-    }
-    template <class T, enable_if_t<is_same<T, string>::value, int> = 0>
-    void write_single(const T& val) {
-        for (char c : val) write_single(c);
-    }
-    template <class T, enable_if_t<is_integral<T>::value, int> = 0>
-    void write_single(T val) {
-        if (pos > (1 << 15) - 50) flush();
-        if (val == 0) {
-            write_single('0');
-            return;
-        }
-        if (val < 0) {
-            write_single('-');
-            val = -val;  // todo min
-        }
-        size_t len = 0;
-        while (val) {
-            small[len++] = char('0' + (val % 10));
-            val /= 10;
-        }
-        reverse(small, small + len);
-        memcpy(line + pos, small, len);
-        pos += len;
-    }
-    template <class T> void write_single(const V<T>& val) {
-        auto n = val.size();
-        for (size_t i = 0; i < n; i++) {
-            if (i) write_single(' ');
-            write_single(val[i]);
-        }
-    }
-};
-
-#pragma endregion
-
-/*
 int main() {
+    Scanner sc = Scanner(stdin);
+    Printer pr = Printer(stdout);
     return 0;
 }
-*/
+
