1e9b8dfd52ac577392a0b5daad989404a3d18565
Kohei Morita <yosupo06@gmail.com>
2020-02-08 03:46:11 +0900

    add test

---
+++ test-oj/bimaching.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
+
+#include "base.h"
+#include "util/fast_io.h"
+#include "graph/bimaching.h"
+
+int main() {
+    Scanner sc(stdin);
+    Printer pr(stdout);
+    int l, r, m;
+    sc.read(l, r, m);
+    V<pair<int, int>> edges(m);
+    for (int i = 0; i < m; i++) {
+        sc.read(edges[i].first, edges[i].second);
+    }
+    BipartiteMaching bm(l, r, edges);
+    V<pair<int, int>> ans;
+    for (int i = 0; i < l; i++) {
+        if (bm.lmt[i] != -1) {
+            ans.push_back({i, bm.lmt[i]});
+        }
+    }
+    pr.writeln(ans.size());
+    for (auto p : ans) {
+        pr.writeln(p.first, p.second);
+    }
+    return 0;
+}
