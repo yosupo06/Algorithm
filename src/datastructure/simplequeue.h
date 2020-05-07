79a205ebdf908c605a012f113164b09cee2b43de
Kohei Morita <yosupo06@gmail.com>
2020-05-07 22:49:12 +0900

    fix

--- src/datastructure/simplequeue.h
+++ src/datastructure/simplequeue.h
@@ -3,7 +3,7 @@
 template <class T> struct SimpleQueue {
     V<T> payload;
     size_t pos = 0;
-    void reserve(size_t) { payload.reserve(size); }
+    void reserve(size_t size) { payload.reserve(size); }
     bool empty() const { return pos == payload.size(); }
     void push(const T& t) { payload.push_back(t); }
     T& front() {
