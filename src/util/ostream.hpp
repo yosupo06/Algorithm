03975d8f858e2fc6baf05c609beba286988dcde0
Kohei Morita <yosupo06@gmail.com>
2020-05-24 20:58:26 +0900

    rm ostream

--- src/util/ostream.hpp
+++
@@ -1,8 +0,0 @@
-ostream &operator<<(ostream &o, __int128_t x) {
-    if (x == 0) return o << 0;
-    if (x < 0) o << '-', x = -x;
-    deque<int> ds;
-    while (x) ds.push_front(x % 10), x /= 10;
-    for (int d: ds) o << d;
-    return o;
-}
